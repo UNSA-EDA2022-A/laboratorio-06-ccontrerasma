14cee6f092c67776eda331269d9ecf50fbb02c6f
ccontrerasma <88167184+ccontrerasma@users.noreply.github.com>
2022-07-14 21:24:29 -0500

    Se agrega ejercicio 3

--- src/main/java/com/example/project/Exercise3.java
+++ src/main/java/com/example/project/Exercise3.java
@@ -19,7 +19,28 @@ public class Exercise3 {
 
 
     public <T extends Comparable<T>> boolean bstEstrictamenteBinario(BST<T> a){
-
-        return false;
+    	//si el arbolesta vacio entonces si ser binario
+        if(a.isEmpty()) {
+    		return true;
+    		
+        //Si no esta vacio, se verificara que los nodos tengan hijos o sean hojas
+    	} 
+        else {
+    		return siBinario(a.root);
+    	}
+    }
+    public <T extends Comparable<T>> boolean siBinario(Node<T> node){
+    	//Primer caso, si los dos hijos son null entonces devolvera true
+    	if(node.left == null && node.right == null) {
+    		return true;
+    		} 
+        //Segundo caso si un hijo es null y el otro no devolvera false por que ya no seria binario
+    	else if ((node.left == null && node.right != null) || (node.left != null && node.right == null)) {
+    		return false;
+    		}
+        //Tercer caso si el nodo presenta los dos hijos diferentes de null se verificara que cada hijo sea nodo u hoja
+    	else {
+    		return siBinario(node.right) && siBinario(node.left);
+    	}
     }
 }
